f7bbd5d97ee3868392d53d0d68a9f67d11b7820a
Atryom <238136@mail.ru>
2021-07-14 23:33:58 +0300

    Add sendForm

--- js/main.js
+++ js/main.js
@@ -9,3 +9,45 @@ const mySwiper = new Swiper('.swiper-container', {
 		el: '.swiper-pagination',
 	}
 });
+
+
+const footerForm = document.querySelector('.footer-form');
+
+
+// отправка данных из формы
+const sendData = (data, cb) => {
+
+  fetch('http://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json; charset=UTF-8',
+    },
+    body: JSON.stringify(data),
+  })
+  .then(response => response.json())
+  .then(cb);
+}
+
+// успешная отправка, калбэк
+const showThanks = () => {
+  alert('You was subscribe');
+};
+
+
+
+// эвенты
+
+
+// сабмит формы футера
+footerForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const formData = new FormData(footerForm);
+  const data = {};
+
+  for (let [elem, value] of formData) {
+    data[elem] = value;
+  }
+
+  sendData(data, showThanks)
+})
